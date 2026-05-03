4c7cefcb0e253b091bbbd92a6ac2f55b04c6eaba
DevJonasMax <jonas.max.dev@gmail.com>
2026-05-03 19:16:23 -0400

    fix: remove base path in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portifolio",
-  assetPrefix: "/portifolio",
   images: {
     unoptimized: true,
   },
